af9be07e17e414f0b7fa774c477aafb8f5b872de
Clarice Bascou <clarice.bascou@gmail.com>
2023-11-03 18:45:18 +0100

    First commit

---
+++ login.js
@@ -0,0 +1,47 @@
+// Insert your code here
+
+document.querySelector('#register').addEventListener('click', function (){
+    const name = document.querySelector('#registerName').value;
+    const email = document.querySelector('#registerEmail').value;
+    const password = document.querySelector('#registerPassword').value;
+
+    
+    fetch('http://localhost:3000/users/signup', {
+        method: 'POST',
+        headers : {'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            name,
+            email,
+            password
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+        
+            if (data.result === true) {
+                window.location.assign('http://127.0.0.1:5500/Projets/lacapsule-fullstack-js/week3/weatherapp-part4/frontend/index.html')
+            }
+        })
+});
+
+document.querySelector('#connection').addEventListener('click', function (){
+    const email = document.querySelector('#connectionEmail').value;
+    const password = document.querySelector('#connectionPassword').value;
+
+    fetch('http://localhost:3000/users/signin', {
+        method: 'POST',
+        headers : {'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            email,
+            password
+        })
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log(data);
+            if (data.result === true) {
+                window.location.assign('http://127.0.0.1:5500/Projets/lacapsule-fullstack-js/week3/weatherapp-part4/frontend/index.html')
+            }
+        })
+
+});
